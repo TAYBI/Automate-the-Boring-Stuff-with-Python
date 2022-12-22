d5da3de33f79c8033e001cdbadc5ab7b0574ee1c
Bilal <taybib4@gmail.com>
2022-12-22 12:40:15 +0100

    make the date detection project

--- Chapter 7 – Pattern Matching with Regular Expressions/07_DateDetection.py
+++ Chapter 7 – Pattern Matching with Regular Expressions/07_DateDetection.py
@@ -1,15 +1,47 @@
-# The ? matches zero or one of the preceding group.
-# The * matches zero or more of the preceding group.
-# The + matches one or more of the preceding group.
-# The {n} matches exactly n of the preceding group.
-# The {n,} matches n or more of the preceding group.
-# The {,m} matches 0 to m of the preceding group.
-# The {n,m} matches at least n and at most m of the preceding group.
-# {n,m}? or *? or +? performs a non-greedy match of the preceding group.
-# ^spam means the string must begin with spam.
-# spam$ means the string must end with spam.
-# The . matches any character, except newline characters.
-# \d, \w, and \s match a digit, word, or space character, respectively.
-# \D, \W, and \S match anything except a digit, word, or space character, respectively.
-# [abc] matches any character between the brackets (such as a, b, or c).
-# [^abc] matches any character that isn’t between the brackets.
+import re
+
+
+# regular Expression to find the date
+regExp = re.compile(r'''
+      (0[0-9]|1[0-9]|2[0-9]|3[0-1])
+      /
+      (0[0-9]|1[0-2])
+      /
+      (1\d{3}|2\d{3})
+''', re.VERBOSE)
+
+
+datesList = []
+
+mo = regExp.findall(
+    '01/12/2012/ bla bla bla bla 31/02/2018 bla bla bla bla 28/02/2018 bla bla bla bla 31/06/2000   bla bla bla bla 17/04/2018')
+
+
+def validDate(day, month, year):
+
+    # February has 28 days
+    if month == 2:
+        if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+            if day > 29:
+                return
+        if day > 28:
+            return False
+
+    # pril, June, September, and November have 30 days
+    if(month == 4 or month == 6 or month == 9 or month == 11):
+        if day > 30:
+            return False
+
+    return True
+
+
+for date in mo:
+    day = int(date[0])
+    month = int(date[1])
+    year = int(date[2])
+
+    if validDate(day, month, year):
+        datesList.append('/'.join(date))
+
+
+print(datesList)
