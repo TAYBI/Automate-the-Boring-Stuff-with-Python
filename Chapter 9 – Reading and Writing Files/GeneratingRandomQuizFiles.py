768fb062c0059d31d956c30c027a156dec428a8d
Bilal <taybib4@gmail.com>
2022-12-24 11:34:07 +0100

    initialize the random quize project

---
+++ Chapter 9 – Reading and Writing Files/GeneratingRandomQuizFiles.py
@@ -0,0 +1,18 @@
+import shelve
+from pathlib import Path
+import pprint
+import os
+
+
+# Here’s what the program does:
+
+#     The command line argument for the keyword is checked.
+#     If the argument is save, then the clipboard contents are saved to the keyword.
+#     If the argument is list, then all the keywords are copied to the clipboard.
+#     Otherwise, the text for the keyword is copied to the clipboard.
+
+# This means the code will need to do the following:
+
+#     Read the command line arguments from sys.argv.
+#     Read and write to the clipboard.
+#     Save and load to a shelf file.
