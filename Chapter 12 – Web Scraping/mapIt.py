015ea71ff6ba1e3aabb22d90115bc21f0a0e0107
Bilal <taybib4@gmail.com>
2023-01-02 12:03:24 +0100

    Creating the Mapit project: launch a new browser to a specified URL

---
+++ Chapter 12 – Web Scraping/mapIt.py
@@ -0,0 +1,16 @@
+import sys
+import webbrowser
+import pyperclip
+
+
+if (len(sys.argv) > 1):
+    # Get address from command line.
+    adress = ''.join(sys.argv[1:])
+
+else:
+    # 3 Get the adress from clipboard
+    # 870 Valencia St, San Francisco, CA 9   4110
+    # You should copy this before you ruhn  the program
+    adress = pyperclip.paste()
+
+webbrowser.open(f'https://www.google.com/maps/place/{adress}')
