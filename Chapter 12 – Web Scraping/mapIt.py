cd9818700be9370a335dadb7a93d75e7991e9a89
Bilal <taybib4@gmail.com>
2023-01-03 13:21:09 +0100

    Resolve merge conflicts

--- Chapter 12 – Web Scraping/mapIt.py
+++ Chapter 12 – Web Scraping/mapIt.py
@@ -12,13 +12,3 @@ else:
     # 870 Valencia St, San Francisco, CA 9   4110
     # You should copy this before you ruhn  the program
     adress = pyperclip.paste()
-
-<<<<<<< HEAD
-webbrowser.open('https://www.google.com/maps/place/' + adress)
-=======
-<<<<<<< HEAD
-webbrowser.open('https://www.google.com/maps/place/' + adress)
-=======
-webbrowser.open(f'https://www.google.com/maps/place/{adress}')
->>>>>>> 015ea71ff6ba1e3aabb22d90115bc21f0a0e0107
->>>>>>> f481e49e59f9006e8c9b67fa303c757ca7dc1152
