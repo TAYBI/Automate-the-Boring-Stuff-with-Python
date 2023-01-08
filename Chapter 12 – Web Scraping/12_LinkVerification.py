d7b59f75b6f62865b67202a7521324a62e3ae26b
Bilal <taybib4@gmail.com>
2023-01-08 21:40:07 +0100

    Start the Link Verification project

---
+++ Chapter 12 – Web Scraping/12_LinkVerification.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import requests
+import bs4
+
+# TODO: take URL of a web page
+# url = input('type your websile: ')
+url = 'https://www.bdodigital.com'
+if not url.startswith('http'):
+    url = 'https://' + url
+
+print('Downloading the site...' + url)
+res = requests.get(url)
+if res.status_code == requests.codes.ok:
+    print('ok')
+
+
+# TODO: download every linked page on the page
+# TODO: flag any pages that have a 404 status
+# TODO: print them out as broken links.
