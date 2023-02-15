ce1a54a7c1c476c07cc4b0a2abd40e641d2d399b
Bilal <taybib4@gmail.com>
2023-02-15 11:13:17 +0100

    Populate the Data Structure

--- Chapter 13 – Working with Excel Spreadsheets/12_ReadingDataFromSpreadsheet.py
+++ Chapter 13 – Working with Excel Spreadsheets/12_ReadingDataFromSpreadsheet.py
@@ -12,8 +12,15 @@ print('Reading Rowas...')
 
 for row in range(2, sheet.max_row + 1):
     # Each row in the spreadsheet has data for one census tract.
-    stat = sheet['B' + str(row)].vaue
+    state = sheet['B' + str(row)].vaue
     country = sheet['C' + str(row)].vaue
     pop = sheet['D' + str(row)].vaue
-
+    # Make sure the key for this state exists.
+    countyData.setdefault(state, {})
+    # Make sure the key for this county in this state exists.
+    countyData[state].setdefault(country, {'tracks': 0, 'pop': 0})
+    # Each row represents one census tract, so increment by one.
+    countyData[state][country]['tracks'] += 1
+    # Increase the county pop by the pop in this census tract.
+    countyData[state][pop]['pop'] += int(pop)
  # TODO: Open a new text file and write the contents of countyData to it.
