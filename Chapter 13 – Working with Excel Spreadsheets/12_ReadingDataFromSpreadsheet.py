28450a2b932d670a92c5efd0ac5aa66395ee9b48
Bilal <taybib4@gmail.com>
2023-02-16 00:10:30 +0100

    Cleaning code and fixin some esue

--- Chapter 13 – Working with Excel Spreadsheets/12_ReadingDataFromSpreadsheet.py
+++ Chapter 13 – Working with Excel Spreadsheets/12_ReadingDataFromSpreadsheet.py
@@ -2,27 +2,28 @@ import openpyxl
 import pprint
 
 print('Openning workbook....')
-wb = openpyxl.load_workbook('produceSales.xlsx')
-
-sheet = wb['Population by cencus Tract']
+wb = openpyxl.load_workbook('censuspopdata.xlsx')
+sheet = wb['Population by Census Tract']
 countyData = {}
 
-# Fill in countyData with each county's population and tracts.
 print('Reading Rowas...')
 
 for row in range(2, sheet.max_row + 1):
     # Each row in the spreadsheet has data for one census tract.
-    state = sheet['B' + str(row)].vaue
-    country = sheet['C' + str(row)].vaue
-    pop = sheet['D' + str(row)].vaue
+    state = sheet['B' + str(row)].value
+    country = sheet['C' + str(row)].value
+    pop = sheet['D' + str(row)].value
+
     # Make sure the key for this state exists.
     countyData.setdefault(state, {})
     # Make sure the key for this county in this state exists.
     countyData[state].setdefault(country, {'tracks': 0, 'pop': 0})
+
     # Each row represents one census tract, so increment by one.
     countyData[state][country]['tracks'] += 1
     # Increase the county pop by the pop in this census tract.
-    countyData[state][pop]['pop'] += int(pop)
+    countyData[state][country]['pop'] += int(pop)
+
 # Open a new text file and write the contents of countyData to it.
 print('Writing results...')
 resultFile = open('census2010.py', 'w')
