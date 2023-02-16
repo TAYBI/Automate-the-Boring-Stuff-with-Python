fb64de1186587582dbca2b57ded44405439190ba
Bilal <taybib4@gmail.com>
2023-02-16 16:39:48 +0100

    Check All Rows and Update Incorrect Prices

--- Chapter 13 – Working with Excel Spreadsheets/12_UpdatingSpreadsheet.py
+++ Chapter 13 – Working with Excel Spreadsheets/12_UpdatingSpreadsheet.py
@@ -1,12 +1,18 @@
 import openpyxl
 
 wb = openpyxl.load_workbook('produceSales.xlsx')
-shhet = wb['Sheet']
+sheet = wb['Sheet']
 
-Price_UPDATE = {
+PRICE_UPDATE = {
     'Garlic': 3.07,
     'Celery': 1.19,
     'Lemon': 1.27
 }
 
-# TODO: Loop through the rows and update the prices.
+# Loop through the rows and update the prices.
+for rowNum in range(2, sheet.max_row):
+    produceName = sheet.cell(row=rowNum, column=1).value
+    if produceName in PRICE_UPDATE:
+        sheet.cell(row=rowNum, column=2).value = PRICE_UPDATE[produceName]
+
+wb.save('updatedProduceSales.xlsx')
