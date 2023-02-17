79fa50e37e01fc99e8d727b9e62cff87f4faada0
Bilal <taybib4@gmail.com>
2023-02-17 10:24:34 +0100

    save the result in excel spreadsheet

--- Chapter 13 – Working with Excel Spreadsheets/multiplicationTable.py
+++ Chapter 13 – Working with Excel Spreadsheets/multiplicationTable.py
@@ -1,8 +1,18 @@
 import sys
+import openpyxl
+from openpyxl.utils import get_column_letter, column_index_from_string
+wb = openpyxl.Workbook()
+sheet = wb.active
 
 number = int(sys.argv[1])
 
+
 for i in range(1, number):
+    letter = get_column_letter(i)
     for j in range(1, number):
-        print(str(i*j) + "\t", end="")
-    print()
+        # sheet[letter + str(i)] = i*j
+        # print(letter + str(j) + f'={str(i*j)}\t', end="")
+        sheet[letter + str(j)] = str(i*j)
+    # print()
+
+wb.save(f'multiplicationTable{number}.xlsx')
