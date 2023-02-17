2eb4b3deb07b84ddda454d7e1e8b9293b1f7451c
Bilal <taybib4@gmail.com>
2023-02-17 12:39:37 +0100

    extracting the data and save it into a dictionary

--- Chapter 13 – Working with Excel Spreadsheets/SpreadsheetCellInverter.py
+++ Chapter 13 – Working with Excel Spreadsheets/SpreadsheetCellInverter.py
@@ -1,14 +1,25 @@
 import openpyxl
+from openpyxl.utils import get_column_letter
 
+print('openning workbook...')
+wb = openpyxl.load_workbook('spreadsheet.xlsx')
 
-items1 = ['ITEM', 'Eggplant', 'Cucumber', 'Green cabl',
-          'Eggplant', 'Garlic', 'Parsnips', 'Asparagus', 'Avocados']
-items2 = ['SOLD', '334', '252', '238', '516', '98', '16', '335', '84']
-wb = openpyxl.Workbook()
-sheet = wb.active
+sheet = wb['Sheet']
+columns = []
+rows = []
 
-for i in range(1, 9):
-    sheet['A' + str(i)] = items1[i - 1]
-    sheet['B' + str(i)] = items2[i - 1]
+for column in range(1, sheet.max_column + 1):
+    columns.append(get_column_letter(column))
+for row in range(1, sheet.max_row + 1):
+    rows.append(row)
 
-wb.save('spreadsheet.xlsx')
+DATA = {}
+
+for column in columns:
+    for row in rows:
+        key = f'{column}{row}'
+        value = sheet[f'{column}{row}'].value
+        DATA[key] = value
+
+for key, value in DATA.items():
+    print(key, ':', value)
